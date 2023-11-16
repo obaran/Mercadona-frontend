77ba8179078321d1573868d37a39753597fc8596
obaran <o.baranonur@gmail.com>
2023-11-16 01:41:07 +0100

    Correction of the code organization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { AuthService } from './services/auth.service'; 
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { ProductService } from './services/product.service';
+import { LoginModalComponent } from './login-modal/login-modal.component';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,7 @@ export class AppComponent {
   username: string = '';
   password: string = '';
   bsModalRef!: BsModalRef;
-  @ViewChild('loginModalTemplate') loginModalTemplate!: TemplateRef<any>;
+  // @ViewChild('loginModalTemplate') loginModalTemplate!: TemplateRef<LoginModalComponent>;
 
 
   constructor(private router: Router, private authService: AuthService,
@@ -30,27 +31,9 @@ export class AppComponent {
     }
 
   openLoginModal() {
-  this.bsModalRef = this.modalService.show(this.loginModalTemplate); 
+  this.bsModalRef = this.modalService.show(LoginModalComponent); 
   }
 
-  onLogin() {
-    this.authService.login(this.username, this.password).subscribe({
-      next: (result) => {
-        if (result) {
-          // Cacher la modal après la connexion réussie
-      this.bsModalRef.hide();
-      this.router.navigate(['/product-list'])
-        } else {
-          // Afficher un message d'erreur
-          alert('Nom d’utilisateur ou mot de passe incorrect.');
-        }
-      },
-      error: (error) => {
-        // Gérer l'erreur de connexion ici
-       console.log(error);
-      }
-    });
-  }
 
   topFunction() {
     window.scroll({ top: 0, left: 0, behavior: 'smooth' });
